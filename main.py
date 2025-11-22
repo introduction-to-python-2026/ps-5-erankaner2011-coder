54e81f4b8b57da61747abd1c41da1761ce88a486
erankaner2011-coder <erankaner2011@gmail.com>
2025-11-22 18:10:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import count_atoms_in_molecule from string_utils.py
 import count_atoms_in_molecule from string_utils.py
-import count_atoms_in_molecule from string_utils.py
+import split_at_first_digit from string_utils.py
 import split_before_each_uppercases from string_utils.py
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
